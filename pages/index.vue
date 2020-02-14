36a9144fddc75a9d5321f935ab8fe0b23b133906
Marvin Kleinpass <marvin.kl@icloud.com>
2020-02-14 17:27:23 +0100

    Landingpage

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,41 @@
+
+
+
+
+
 <template>
   <div class="container">
     <div>
-      <logo />
       <h1 class="title">
-        chat-frontend
+        Pinguinapp
       </h1>
-      <h2 class="subtitle">
-        barrier-free chat application
-      </h2>
+      <logo />
+
+      <h4 class="subtitle">
+        <p>sentence1<br />
+          sentence2
+          sentence3
+
+
+        </p>
+
+      </h4>
       <div class="links">
         <a
           href="https://nuxtjs.org/"
           target="_blank"
           class="button--green"
         >
-          Documentation
+          Los geht's
         </a>
-        <a
+        <!-- <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
         >
           GitHub
-        </a>
-        <nuxt-link to="/select-question">Home page</nuxt-link>
+        </a> -->
+        <!-- <nuxt-link to="/select-question">Home page</nuxt-link> -->
       </div>
     </div>
   </div>
